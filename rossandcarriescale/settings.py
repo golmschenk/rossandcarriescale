b0e8445a4becb8d33ad85b73507891d1bc9411fd
Greg Olmschenk <golmschenk@gmail.com>
2015-09-22 15:43:18 -0400

    Database setup with secret file

--- rossandcarriescale/settings.py
+++ rossandcarriescale/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+from rossandcarriescale.secret import secret_key, database_name, database_user, database_password
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gl_ojq_80grhtyp%4*%d8ou*v))u%_n8a^k_q_wk^hna%9sk=-'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,8 +79,12 @@ WSGI_APPLICATION = 'rossandcarriescale.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': database_name,
+        'USER': database_user,
+        'PASSWORD': database_password,
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
---
+++ rossandcarriescale/template.secret.py
@@ -0,0 +1,4 @@
+secret_key = ''
+database_name = ''
+database_user = ''
+database_password = ''
