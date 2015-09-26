79a92a3667bb55e611b5b1a3ad4bdfa6c3235304
Greg Olmschenk <golmschenk@gmail.com>
2015-09-25 20:07:27 -0400

    Refactor to template

--- scales/tests/test_views.py
+++ scales/tests/test_views.py
@@ -5,4 +5,4 @@ class TestScalesView(TestCase):
 
     def test_front_page_has_title(self):
         response = self.client.get('/')
-        self.assertIn(b'<title>Ross and Carrie Scale</title>', response.content)
+        self.assertTemplateUsed(response, 'scales.html')
--- scales/views.py
+++ scales/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def scales_page(request):
-    return HttpResponse(b'<title>Ross and Carrie Scale</title>')
+    return render(request, 'scales.html')
